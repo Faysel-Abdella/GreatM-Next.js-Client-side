92be09861492ed63a8c6d08285eab3f667a88ee2
Faysel Abdella <fayselcode@gmail.com>
2024-10-16 21:52:02 +0300

    Add contents

---
+++ src/assets/index.ts
@@ -0,0 +1,35 @@
+import member from "@/assets/icons/sidebar/member.svg";
+import memberActive from "@/assets/icons/sidebar/member-active.svg";
+import payment from "@/assets/icons/sidebar/payment.svg";
+import paymentActive from "@/assets/icons/sidebar/payment-active.svg";
+import content from "@/assets/icons/sidebar/content.svg";
+import contentActive from "@/assets/icons/sidebar/content-active.svg";
+import community from "@/assets/icons/sidebar/community.svg";
+import communityActive from "@/assets/icons/sidebar/community-active.svg";
+import message from "@/assets/icons/sidebar/message.svg";
+import messageActive from "@/assets/icons/sidebar/message-active.svg";
+import notice from "@/assets/icons/sidebar/notice.svg";
+import noticeActive from "@/assets/icons/sidebar/notice-active.svg";
+import faq from "@/assets/icons/sidebar/faq.svg";
+import faqActive from "@/assets/icons/sidebar/faq-active.svg";
+import oneToOne from "@/assets/icons/sidebar/one-to-one.svg";
+import oneToOneActive from "@/assets/icons/sidebar/one-to-one-active.svg";
+
+export default {
+  member,
+  memberActive,
+  payment,
+  paymentActive,
+  content,
+  contentActive,
+  community,
+  communityActive,
+  message,
+  messageActive,
+  notice,
+  noticeActive,
+  faq,
+  faqActive,
+  oneToOne,
+  oneToOneActive,
+};
