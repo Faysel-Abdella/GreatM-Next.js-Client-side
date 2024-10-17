3133046ab95675cbe9aa010ad2a8a4dc0c1d2e8a
Faysel Abdella <fayselcode@gmail.com>
2024-10-17 23:52:48 +0300

    add new

---
+++ src/app/(pages)/admin/(콘텐츠 관리)/content/category-management/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CategoryManagement = () => {
+  return (
+    <section className="font-noto">
+      <header>
+        <div className="flex justify-end">
+          <button className=" text-mainGray font-normal text-base text-right">
+            로그아웃
+          </button>
+        </div>
+        <h2 className="mt-6 font-bold text-[30px] leading-[42px] text-mainBlack">
+          카테고리 관리
+        </h2>
+      </header>
+    </section>
+  );
+};
+
+export default CategoryManagement;
